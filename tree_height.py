99c8439ee3c34fadf69739a2df042dbc1984ddbb
EmilsK221RDB241 <125762817+EmilsK221RDB241@users.noreply.github.com>
2023-03-04 17:46:06 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -6,14 +6,28 @@ import numpy
 
 
 def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
-
+    nodes = [[] for _ in range(n)]
+    
+    heights = [0] * n
+    for child_idx in range(n):
+        parent_idx = parents[child_idx]
+        if parent_idx == -1:
+            root = child_idx
+        else:
+            nodes[parent_idx].append(child_idx)
+    def compute_node_height(node_idx):
+        if not nodes[node_idx]:
+            return 1
+        child_heights = [compute_node_height(child_idx) for child_idx in nodes[node_idx]]
+        return max(child_heights) + 1
+    return compute_node_height(root)
 
 def main():
     # implement input form keyboard and from files
+    n = int(input())
+    parents = list(map(int, input().split()))
+    height = compute_height(n, parents)
+    print(height)
     
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
